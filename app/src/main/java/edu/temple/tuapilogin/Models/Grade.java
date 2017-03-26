b637bf0d87badcfcb84bce326755065026cf66df
laurenlezberg <tud30354@temple.edu>
2017-03-26 13:32:34 -0400

    Added grades model

---
+++ app/src/main/java/edu/temple/tuapilogin/Models/Grade.java
@@ -0,0 +1,35 @@
+package edu.temple.tuapilogin.Models;
+
+/**
+ * Created by laurenlezberg on 3/26/17.
+ */
+
+import android.support.annotation.Nullable;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Grade {
+    private String name;
+    private String grade;
+    private String updated;
+
+    private Grade(String name, String grade, String updated){
+        this.name=name;
+        this.grade=grade;
+        this.updated=updated;
+    }
+
+    @Nullable
+    public static Grade createGrade(JSONObject jsonObject) throws JSONException {
+        String name = jsonObject.getString("name");
+        String grade = jsonObject.getString("value");
+        String updated = jsonObject.getString("updated");
+
+        if (name != null && grade != null && updated != null)
+            return new Grade(name, grade, updated);
+        return null;
+    }
+
+
+}
