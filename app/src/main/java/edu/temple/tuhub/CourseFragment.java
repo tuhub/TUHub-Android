5dc920c59065ef1d179f37c7ac4fbca634c2c1b9
Connor Crawford <cdcraw@icloud.com>
2017-03-26 16:44:58 -0400

    Adds fragments for courses (and a placeholder fragment for calendar), fixes errors in MainActivity

--- app/src/main/java/edu/temple/tuhub/CourseFragment.java
+++ app/src/main/java/edu/temple/tuhub/CourseFragment.java
@@ -1,35 +1,23 @@
 package edu.temple.tuhub;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import edu.temple.tuhub.models.Course;
+
 
 /**
  * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link CourseFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
  * Use the {@link CourseFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class CourseFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
 
     public CourseFragment() {
         // Required empty public constructor
@@ -39,17 +27,12 @@ public class CourseFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment CourseFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static CourseFragment newInstance(String param1, String param2) {
+    public static CourseFragment newInstance() {
         CourseFragment fragment = new CourseFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+//        fragment.setArguments(args);
         return fragment;
     }
 
@@ -57,8 +40,8 @@ public class CourseFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
@@ -66,51 +49,15 @@ public class CourseFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_course, container, false);
-    }
+        final View view = inflater.inflate(R.layout.fragment_course, container, false);
+        final ViewPager viewPager = (ViewPager) view.findViewById(R.id.view_pager);
+        viewPager.setAdapter(new ViewPagerAdapter(getFragmentManager()));
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        return view;
     }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
-
 
     public static class ViewPagerAdapter extends FragmentPagerAdapter {
-        private static int NUM_ITEMS = 3;
+        private static int NUM_ITEMS = 2;
 
         public ViewPagerAdapter(FragmentManager fragmentManager) {
             super(fragmentManager);
@@ -124,14 +71,12 @@ public class CourseFragment extends Fragment {
 
 
         @Override
-        public android.support.v4.app.Fragment getItem(int position) {
+        public Fragment getItem(int position) {
             switch (position) {
-//                case 0: // Fragment # 0 - This will show FirstFragment
-//                    return FirstFragment.newInstance(0, "Page # 1");
-//                case 1: // Fragment # 0 - This will show FirstFragment different title
-//                    return FirstFragment.newInstance(1, "Page # 2");
-//                case 2: // Fragment # 1 - This will show SecondFragment
-//                    return SecondFragment.newInstance(2, "Page # 3");
+                case 0: // Fragment # 0 - This will show FirstFragment
+                    return CourseCalendarFragment.newInstance();
+                case 1: // Fragment # 0 - This will show FirstFragment different title
+                    return CourseListFragment.newInstance(1);
                 default:
                     return null;
             }
@@ -140,7 +85,14 @@ public class CourseFragment extends Fragment {
         // Returns the page title for the top indicator
         @Override
         public CharSequence getPageTitle(int position) {
-            return "Page " + position;
+            switch (position) {
+                case 0:
+                    return "Calendar";
+                case 1:
+                    return "List";
+                default:
+                    return null;
+            }
         }
 
     }
--- app/src/main/java/edu/temple/tuhub/MainActivity.java
+++ app/src/main/java/edu/temple/tuhub/MainActivity.java
@@ -1,64 +1,56 @@
 package edu.temple.tuhub;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
-public class MainActivity extends AppCompatActivity {
 
-    public String user, tuId, password;
+import edu.temple.tuhub.models.Course;
+
+public class MainActivity extends AppCompatActivity implements CourseListFragment.OnListFragmentInteractionListener {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_courses:
+                    loadFragment(R.id.content, CourseFragment.newInstance(), false, true);
+                    return true;
+                case R.id.navigation_marketplace:
+                    // TODO: Load fragment
+                    return true;
+                case R.id.navigation_maps:
+                    // TODO: Load fragment
+                    return true;
+                case R.id.navigation_news:
+                    // TODO: Load fragment
+                    return true;
+                case R.id.navigation_more:
+                    // TODO: Load fragment
+                    return true;
+            }
+            return false;
+        }
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tab_bar);
+        setContentView(R.layout.activity_main);
 
-        Bundle extras = getIntent().getExtras();
-        if (extras == null) {
-            user = "";
-            tuId = "";
-            password = "";
-        } else {
-            user = extras.getString("user");
-            tuId = extras.getString("tuId");
-            password = extras.getString("password");
-        }
-
-        final BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation_view);
-
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment fragment = null;
-                switch (item.getItemId()) {
-                    case R.id.courses:
-                        //Load fragment
-                        break;
-                    case R.id.marketplace:
-                        // Load appropriate fragment
-                        break;
-                    case R.id.maps:
-                        // Load appropriate fragment
-                        break;
-                    case R.id.news:
-                        // Load appropriate fragment
-                        break;
-                    case R.id.account:
-                        // Load appropriate fragment
-                        break;
-                }
-                return true;
-            }
-        });
-
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
     private void loadFragment(int ID, Fragment fragment, boolean backStack, boolean clearBackStack) {
-        FragmentManager fm = getFragmentManager();
+        FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction ft = fm.beginTransaction().replace(ID, fragment);
         if(clearBackStack){
             fm.popBackStack();
@@ -71,4 +63,8 @@ public class MainActivity extends AppCompatActivity {
         fm.executePendingTransactions();
     }
 
+    @Override
+    public void onListFragmentInteraction(Course item) {
+
+    }
 }
