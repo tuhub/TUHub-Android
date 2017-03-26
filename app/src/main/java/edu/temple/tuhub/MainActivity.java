52c48e0ce2de16a48a09c430e17a86a57614e008
Tom <tuf23395@temple.edu>
2017-03-26 14:47:40 -0400

    Bottom Navigation Bar + Intent w/ Extras

--- app/src/main/java/edu/temple/tuhub/MainActivity.java
+++ app/src/main/java/edu/temple/tuhub/MainActivity.java
@@ -1,51 +1,74 @@
 package edu.temple.tuhub;
 
-import android.os.Bundle;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.TextView;
-
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_courses:
-                    mTextMessage.setText(R.string.title_courses);
-                    return true;
-                case R.id.navigation_marketplace:
-                    mTextMessage.setText(R.string.title_marketplace);
-                    return true;
-                case R.id.navigation_maps:
-                    mTextMessage.setText(R.string.title_maps);
-                    return true;
-                case R.id.navigation_news:
-                    mTextMessage.setText(R.string.title_news);
-                    return true;
-                case R.id.navigation_more:
-                    mTextMessage.setText(R.string.title_more);
-                    return true;
-            }
-            return false;
-        }
-
-    };
+    public String user, tuId, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tab_bar);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        Bundle extras = getIntent().getExtras();
+        if (extras == null) {
+            user = "";
+            tuId = "";
+            password = "";
+        } else {
+            user = extras.getString("user");
+            tuId = extras.getString("tuId");
+            password = extras.getString("password");
+        }
+
+        final BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation_view);
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch (item.getItemId()) {
+                    case R.id.courses:
+                        //Load fragment
+                        break;
+                    case R.id.marketplace:
+                        // Load appropriate fragment
+                        break;
+                    case R.id.maps:
+                        // Load appropriate fragment
+                        break;
+                    case R.id.news:
+                        // Load appropriate fragment
+                        break;
+                    case R.id.account:
+                        // Load appropriate fragment
+                        break;
+                }
+                return true;
+            }
+        });
+
+    }
+    private void loadFragment(int ID, Fragment fragment, boolean backStack, boolean clearBackStack) {
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction().replace(ID, fragment);
+        if(clearBackStack){
+            fm.popBackStack();
+            fm.popBackStack();
+        }
+        if (backStack) {
+            ft.addToBackStack(null);
+        }
+        ft.commit();
+        fm.executePendingTransactions();
     }
 
 }
